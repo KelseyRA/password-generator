36797c95dbb8035d0ad0c0605e1d06b81167967f
Luna Rae <kra7888@gmail.com>
2022-12-11 21:18:05 -0500

    Updated JS code with comments. Updated README.

--- Develop/script.js
+++ Develop/script.js
@@ -1,69 +1,110 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var numbers = [0,1,2,3,4,5,6,7,8,9];
+
+// Variables containing possible password character values.
+
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["*", "&", "%", "$", "#","@", "!"];
 var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+console.log(numbers);
+console.log(specialCharacters)
+console.log(lowercaseLetters);
+console.log(uppercaseLetters);
+
+// Variables for user questions.
+
+var includeLowercaseLetters;
+var includeUppercaseLetters;
+var includeNumbers;
+var includeSpecialCharacters;
+
+// Add event listener to generate button
+// Prompts are delivered to the user once the click the 'generate password' button
+
 generateBtn.addEventListener("click", writePassword);
 
-// Prompts are delivered to the user once the click the 'generate password' button
+
 // 5 different prompts are served to the user asking about the structure of the password
 
-
 function generatePassword() {
+  
+  var numberOfCharacters = [];
+  var passwordCharacters = [];
 
-  var password = "";
-  var passwordCharacters = "";
+  // User must pick a length of the password 8-128 characters
 
-  var numberOfCharacters = prompt("How many characters would you like your password to contain?");
-  numberOfCharacters = parseInt(numberOfCharacters);
+  var numberOfCharacters = (prompt("How many characters would you like your password to contain?"));
  
-// User must pick a length of the password 8-128 characters
-if (numberOfCharacters < 8){
-    alert("Password must contain at least 8 characters");
-    return ""
+  numberOfCharacters = numberOfCharacters;
+ 
+// Check to make sure the user has entered a valid length
+
+if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+    alert("Password must contain at least 8 characters and no more than 128");
+    numberOfCharacters = (prompt("How many characters would you like your password to contain?"));
   }
 
-  if (numberOfCharacters > 128) {
-    alert("Password must contain less than 128 characters");
-    return ""
-  }
+  console.log(numberOfCharacters);
 
   // User selects 'ok' or 'cancel' to indicate if that want to include the certain types of characters
 
-  lowercaseLetters = confirm("Do you want to add lowercase letter?");
+  includeLowercaseLetters = confirm("Do you want to add lowercase letter?");
+  includeUppercaseLetters = confirm("Do you want to add uppercase letters?");
+  includeNumbers = confirm("Do you want to add numbers?");
+  includeSpecialCharacters = confirm("Do you want to include special characters?");
 
-    if (lowercaseLetters === true) {
-      passwordCharacters += lowercaseLetters;
+  console.log(includeLowercaseLetters);
+  console.log(includeUppercaseLetters);
+  console.log(includeNumbers);
+  console.log(includeSpecialCharacters);
+
+  // If user selects cancel for all prompts an alert will be shared then the questions will be asked again.
+
+  while (includeLowercaseLetters === false && includeUppercaseLetters === false && includeNumbers === false && includeSpecialCharacters === false) {
+    alert("You have not selected any types of characters a password cannot be generated");
+    includeLowercaseLetters = confirm("Do you want to add lowercase letter?");
+    includeUppercaseLetters = confirm("Do you want to add uppercase letters?");
+    includeNumbers = confirm("Do you want to add numbers?");
+    includeSpecialCharacters = confirm("Do you want to include special characters?");
+  }
+
+    if (includeLowercaseLetters === true) {
+      passwordCharacters = passwordCharacters.concat(lowercaseLetters);
+    }
+    console.log(passwordCharacters);
+
+    if (includeUppercaseLetters === true) {
+      passwordCharacters = passwordCharacters.concat(uppercaseLetters);
     }
 
-  uppercaseLetters = confirm("Do you want to add uppercase letters?");
-
-    if (uppercaseLetters === true) {
-      passwordCharacters += uppercaseLetters;
+    console.log(passwordCharacters);
+    
+    if (includeNumbers === true) {
+      passwordCharacters = passwordCharacters.concat(numbers);
     }
 
-    numbers = confirm("Do you want to add numbers?");
-    if (numbers === true) {
-      passwordCharacters += numbers;
+    console.log(passwordCharacters);
+
+    if (includeSpecialCharacters === true) {
+      passwordCharacters = passwordCharacters.concat(specialCharacters);
     }
 
-    specialCharacters = confirm("Do you want to include special characters?");
+    // a password is created with all indicated characters and the length entered
+    
+    var finishedPassword = [];
 
-    if (specialCharacters === true) {
-      passwordCharacters += specialCharacters;
+    for (var i = 0; i < numberOfCharacters; i++) {
+      finishedPassword = finishedPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)]
+      console.log(finishedPassword);
     }
 
-    for (var i = 0; i < numberOfCharacters; i+1) {
-      password = passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)]
-    }
+    return(finishedPassword);
+
 }
 
-
-// If user selects cancel for all prompts the prompts should loop again
-// a password is created with all indicated characters and the length entered
 // This password is then displayed for the user to see
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -72,5 +113,9 @@ function writePassword() {
 // call other function to make it appear on the screen
 }
 
-// Add event listener to generate button
+
+
+
+
+
 
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# password-generator
+# Password-Generator
+
+## Description 
+
+This application randomly generates a password based on the parameters provided by the user. The password must be between 8-128 characters and can include a mixture of uppercase letters, lowercase, letters, numbers, and special characters. The user will indicate their preference by answering prompts delivered to them.
+
+## User Instructions
+
+The user will need to click on the "Generate Password" button to begin the process. They will be asked to indicate the length of the password, whether they would like to include lowercase letters, uppercase letters, numbers and/or special characters. If a used does not select any of these options an alert message will indicate the password is unable to be generated without any characters. The user will then be asked the same questions over again.  Once this process is completed a password will appear in the "Your Secure Password" text box.
+
+Screenshots are included below.
